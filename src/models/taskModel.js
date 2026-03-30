3358ec3b2e8d9010a856b143cc5c49452b04fef4
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2026-03-31 01:02:24 +0530

    server and models

---
+++ src/models/taskModel.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({});
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
---
+++ src/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please add a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: [6, "Password must be at least 6 characters"],
+    select: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
